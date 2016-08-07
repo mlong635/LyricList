92a51f66cb1fde59089f6fea56cfd72321431692
Matt Long <mlong635@gmail.com>
2016-08-07 00:02:03 -0700

    Add basic hello world html and server files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const router = express.Router();
+
+router.get('/', function(req, res){
+  res.sendFile(path.resolve(__dirname, './index.html'));
+  console.log('connecting to index.html');
+});
+
+app.use(bodyParser.json());
+app.use(router);
+
+app.listen(process.env.PORT || 8888, function(){
+  console.log('LyricList is listening on port 8888');
+});
+
+module.express = app;
