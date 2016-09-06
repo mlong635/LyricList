084c4ef3648e769ea88855904d0dae7b236f397e
Matt Long <mlong635@gmail.com>
2016-09-05 19:15:20 -0700

    Refactor [webpack.config.js] for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
-let webpack = require('webpack');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
-let HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+
+var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   entry: [
-    './client/client.js'
+    'webpack/hot/dev-server',
+    'webpack-hot-middleware/client',
+    path.join(__dirname, 'client/client.js')
   ],
+  output: {
+    path: path.resolve(__dirname + 'dist/'),
+    publicPath: '/',
+    filename: 'index_bundle.js'
+  },
   module: {
     loaders: [
       {
-        test : /\.jsx?$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: "babel-loader"
       },
@@ -23,11 +26,45 @@ module.exports = {
       }
     ]
   },
-  output: {
-    filename: "index_bundle.js",
-    path: __dirname + '/dist'
-  },
-  plugins: [HTMLWebpackPluginConfig]
-};
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  target: 'web'
+}
+
+
+// let webpack = require('webpack');
+// let HtmlWebpackPlugin = require('html-webpack-plugin');
+// let HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+//   template: __dirname + '/index.html',
+//   filename: 'index.html',
+//   inject: 'body'
+// });
+
+// module.exports = {
+//   entry: [
+//     './client/client.js'
+//   ],
+//   module: {
+//     loaders: [
+//       {
+//         test : /\.jsx?$/,
+//         exclude: /node_modules/,
+//         loader: "babel-loader"
+//       },
+//       {
+//         test: /\.css$/,
+//         loader: "style-loader!css-loader"
+//       }
+//     ]
+//   },
+//   output: {
+//     filename: "index_bundle.js",
+//     path: __dirname + '/dist'
+//   },
+//   plugins: [HTMLWebpackPluginConfig]
+// };
 
 
