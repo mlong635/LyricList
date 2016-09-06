8846fc872fed4f128c6947f3b118b02c66cf3416
Matt Long <mlong635@gmail.com>
2016-09-06 09:44:56 -0700

    Refactor [webpack.config.js] Heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,22 @@
 
-var webpack = require('webpack');
-var path = require('path');
-
-module.exports = {
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack-hot-middleware/client',
-    path.join(__dirname, 'client/client.js')
-  ],
-  output: {
-    path: path.resolve(__dirname + 'dist/'),
-    publicPath: '/',
-    filename: 'index_bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  target: 'web'
-}
-
-
-// let webpack = require('webpack');
-// let HtmlWebpackPlugin = require('html-webpack-plugin');
-// let HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-//   template: __dirname + '/index.html',
-//   filename: 'index.html',
-//   inject: 'body'
-// });
+// var webpack = require('webpack');
+// var path = require('path');
 
 // module.exports = {
 //   entry: [
-//     './client/client.js'
+//     'webpack/hot/dev-server',
+//     'webpack-hot-middleware/client',
+//     path.join(__dirname, 'client/client.js')
 //   ],
+//   output: {
+//     path: path.resolve(__dirname + 'dist/'),
+//     publicPath: '/',
+//     filename: 'index_bundle.js'
+//   },
 //   module: {
 //     loaders: [
 //       {
-//         test : /\.jsx?$/,
+//         test: /\.jsx?$/,
 //         exclude: /node_modules/,
 //         loader: "babel-loader"
 //       },
@@ -60,11 +26,45 @@ module.exports = {
 //       }
 //     ]
 //   },
-//   output: {
-//     filename: "index_bundle.js",
-//     path: __dirname + '/dist'
-//   },
-//   plugins: [HTMLWebpackPluginConfig]
-// };
+//   plugins: [
+//     new webpack.optimize.OccurrenceOrderPlugin(),
+//     new webpack.HotModuleReplacementPlugin(),
+//     new webpack.NoErrorsPlugin()
+//   ],
+//   target: 'web'
+// }
+
+
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './client/client.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test : /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  },
+  output: {
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
 
 
